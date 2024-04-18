2f05653e883ca3295c520bca4832b4b1218f68aa
Marwa Selwaye <75786866+Marwasel@users.noreply.github.com>
2024-04-18 11:41:32 +0200

    txt files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from rouge import Rouge
 
+
 def read_summary(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
@@ -10,10 +11,12 @@ def compute_rouge(human_summary, llm_summary):
     return scores
 
 if __name__ == "__main__":
-    human_summary = read_summary("models/human_summary.txt")
-    llm_summary = read_summary("systems/llm_summary.txt")
+    human_summary = read_summary("manual/colorcast.txt")
+    llm_summary = read_summary("llm/Colorcast.txt")
 
  
 
     rouge_scores = compute_rouge(human_summary, llm_summary)
     print("ROUGE Scores:", rouge_scores)
+
+
